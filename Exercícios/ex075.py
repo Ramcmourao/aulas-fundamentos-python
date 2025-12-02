14bf93aae86a8ec1d398f0c305b6217e195165e3
Seu Nome <91672048+Ramcmourao@users.noreply.github.com>
2025-12-02 14:39:47 +0000

    yes

--- Exercícios/ex075.py
+++ Exercícios/ex075.py
@@ -1,10 +1,16 @@
 '''
 Crie um programa que tenha uma função que receba um número inteiro e mostre a tabuada desse número.
 '''
-
+import random
 def tabuada(num: int):
+    """
+    -> Apresenta a tabuada de um valor.
+    :param num: o valor sobre o qual será apresentada a tabuada
+    :return: sem retorno
+    Desenvolvido por: Ricardo Mourão
+    """
     for c in range(1, 11):
         print(f'{num} x {c} = {c * num}')
 
 
-tabuada(7)
+help(random)
